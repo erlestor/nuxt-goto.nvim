6ffb79ad5efd283882ca458a67291fbd41110332
Erlend Storsve <erlendstorsve@gmail.com>
2025-10-13 02:18:44 +0200

    fix: composables, utils and stores

--- lua/goto-alias/init.lua
+++ lua/goto-alias/init.lua
@@ -9,7 +9,7 @@ M.is_nuxt_project = false
 M.original_definition = vim.lsp.buf.definition
 M.nuxt_directory_path = ""
 
-local default_check_directories = { "" }
+local default_check_directories = { "/apps/web", "/apps/nuxt", "/apps/frontend", "/apps/website" }
 
 M.setup = function(opts)
 	opts = opts or {}
@@ -36,22 +36,52 @@ M.setup = function(opts)
 			vim.lsp.buf.definition = M.watch
 		end,
 	})
+
+	-- this is in the original repo. whyyy???
+	-- - maybe for when you change repo without closing neovim?
+	-- M.is_nuxt_project = false
+	-- M.original_definition = vim.lsp.buf.definition
+	-- M.nuxt_directory_path = ""
 end
 
 M.watch = function()
 	M.original_definition()
+
+	if not M.is_nuxt_project then
+		return
+	end
 
 	vim.defer_fn(function()
 		local line = vim.fn.getline(".")
 		local path = string.match(line, '".-/(.-)"')
 		local file = vim.fn.expand("%")
 
-		if string.find(file, "components.d.ts") then
-			vim.api.nvim_buf_delete(0, { force = false })
-			vim.cmd("edit " .. M.nuxt_directory_path .. "/" .. path)
-		elseif string.find(line, "components.d.ts") then
-			vim.cmd("cclose")
-			vim.cmd("edit " .. M.nuxt_directory_path .. "/" .. path)
+		local auto_import_files = { "components.d.ts", "imports.d.ts" }
+
+		for _, auto_import_file in ipairs(auto_import_files) do
+			if string.find(file, auto_import_file) then
+				vim.api.nvim_buf_delete(0, { force = false })
+				vim.cmd("edit " .. M.nuxt_directory_path .. "/" .. path)
+				break
+			elseif string.find(line, auto_import_file) then
+				if string.find(line, "stores") then
+					local storePath = string.match(line, "'.-/(.-)'")
+					vim.cmd("cclose")
+					vim.cmd("edit " .. M.nuxt_directory_path .. "/stores/" .. storePath .. ".ts")
+				elseif string.find(line, "composables") then
+					local composablePath = string.match(line, "'.-/(.-)'")
+					vim.cmd("cclose")
+					vim.cmd("edit " .. M.nuxt_directory_path .. "/composables/" .. composablePath .. ".ts")
+				elseif string.find(line, "utils") then
+					local utilsPath = string.match(line, "'.-/(.-)'")
+					vim.cmd("cclose")
+					vim.cmd("edit " .. M.nuxt_directory_path .. "/utils/" .. utilsPath .. ".ts")
+				else
+					vim.cmd("cclose")
+					vim.cmd("edit " .. M.nuxt_directory_path .. "/" .. path)
+				end
+				break
+			end
 		end
 	end, 100)
 end
